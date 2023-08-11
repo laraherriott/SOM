48ca55722f2d1b1600565763b6bc1d45fbc1a24c
Lara Herriott <58111273+laraherriott@users.noreply.github.com>
2023-08-11 12:26:38 +0100

    Remove unused imports

--- som-prediction/script_to_run.py
+++ som-prediction/script_to_run.py
@@ -3,13 +3,7 @@ import random
 import os
 import sys
 import json
-
-from rdkit import Chem
-from rdkit.Chem import PandasTools
-
 import torch
-import torch.nn as nn
-import torch_geometric.nn as gnn
 
 import sklearn.preprocessing as sklearn_pre
 
